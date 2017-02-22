d7ee365f652dc2c3007806ea8fc2b82f9279dec2
Emilio Wuerges <wuerges@gmail.com>
2017-02-22 18:48:29 -0300

    created package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dispersor',
+      version='0.1',
+      description='Fault-Tolerant, distributed computing library, containing most common algorithms and tools',
+      url='http://github.com/wuerges/dispensor',
+      author='Emilio Wuerges',
+      author_email='wuerges@gmail.com',
+      license='BSD3',
+      packages=['dispensor'],
+      zip_safe=True)
